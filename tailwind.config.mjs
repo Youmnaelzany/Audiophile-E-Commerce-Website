5bde3bd9989d4f99d714a622bcb30c3a9e1b6622
Yaya <youmnael38@gmail.com>
2023-12-26 20:47:33 +0200

    finish header for desktop
    finish header for desktop

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,7 @@ module.exports = {
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
   ],
   prefix: "",
   theme: {
@@ -16,6 +16,33 @@ module.exports = {
         "2xl": "1400px",
       },
     },
+    screens: {
+      'sm': { 'min': '320px', 'max': '480px' },
+      // => @media (min-width: 320px and max-width: 480px) { ... }
+
+      'md': { 'min': '481px', 'max': '768px' },
+      // => @media (min-width: 481px and max-width: 768px) { ... }
+
+      'lg': { 'min': '769px', 'max': '1024px' },
+      // => @media (min-width: 769px and max-width: 1024px) { ... }
+
+      'xl': { 'min': '1025px', 'max': '1200px' },
+      // => @media (min-width: 1025px and max-width: 1200px) { ... }
+
+      '2xl': { 'max': '1400px' },
+      // => @media (min-width: 1536px) { ... }
+    },
+    colors: {
+      'dark-peach': '#D87D4A',
+      'peach': '#FBAF85',
+      'dark-black': '#101010',
+      'black': '#000000',
+      'black-bg': '#0E0E0E',
+      'dark-white': '#F1F1F1',
+      'light-white': '#FAFAFA',
+      'white': '#FFFFFF',
+    },
+
     extend: {
     },
   },
